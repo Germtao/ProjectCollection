ae2bb592ccd22aea0ddae7f224f9f882350a7c6e
QDSG <piftao1127@163.com>
2019-05-16 17:26:30 +0800

    【QRCode】二维码生成

--- QRCode/QRCode/Classes/GenerateQRcode/Controller/GenerateQRcodeViewController.swift
+++ QRCode/QRCode/Classes/GenerateQRcode/Controller/GenerateQRcodeViewController.swift
@@ -7,24 +7,31 @@
 //
 
 import UIKit
+import CoreImage
 
 class GenerateQRcodeViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var qrcodeIconView: UIImageView!
+    @IBOutlet weak var inputTextView: UITextView!
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // 1. 创建二维码滤镜
+        let filter = CIFilter(name: "CIQRCodeGenerator")
+        filter?.setDefaults() // 恢复滤镜默认设置
+        
+        // 2. 设置滤镜输入数据
+        // KVC
+        let data = "123".data(using: .utf8)
+        filter?.setValue(data, forKey: "inputMessage")
+        
+        // 3. 从二维码滤镜中, 获取结果图片
+        var image = filter?.outputImage
+        let transform = CGAffineTransform(scaleX: 20, y: 20)
+        image = image?.transformed(by: transform)
+        let resultImage = UIImage(ciImage: image!) // 图片处理
+        print(resultImage.size)
+        
+        // 4. 显示图片
+        qrcodeIconView.image = resultImage
     }
-    */
-
 }
