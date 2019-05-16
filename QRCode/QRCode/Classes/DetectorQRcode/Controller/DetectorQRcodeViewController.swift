31b6ef31a3690c4822807bb99d217d054e3740f5
QDSG <piftao1127@163.com>
2019-05-16 17:11:11 +0800

    【QRCode】搭建项目结构

---
+++ QRCode/QRCode/Classes/DetectorQRcode/Controller/DetectorQRcodeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetectorQRcodeViewController.swift
+//  QRCode
+//
+//  Created by QDSG on 2019/5/16.
+//  Copyright © 2019 unitTao. All rights reserved.
+//
+
+import UIKit
+
+class DetectorQRcodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ QRCode/QRCode/Classes/ScanQRcode/Controller/ScanQRcodeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ScanQRcodeViewController.swift
+//  QRCode
+//
+//  Created by QDSG on 2019/5/16.
+//  Copyright © 2019 unitTao. All rights reserved.
+//
+
+import UIKit
+
+class ScanQRcodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
