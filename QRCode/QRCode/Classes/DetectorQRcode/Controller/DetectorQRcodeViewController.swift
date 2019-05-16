71cee1d671b2ba787827d53e49eff723585cfc02
QDSG <piftao1127@163.com>
2019-05-16 19:10:18 +0800

    【QRCode】绘制识别的二维码边框

--- QRCode/QRCode/Classes/DetectorQRcode/Controller/DetectorQRcodeViewController.swift
+++ QRCode/QRCode/Classes/DetectorQRcode/Controller/DetectorQRcodeViewController.swift
@@ -27,12 +27,57 @@ class DetectorQRcodeViewController: UIViewController {
             fatalError("no value of CIDetector.")
         }
         
+        var results = [String]()
         // 2. 直接探测二维码特征
         let features = detector.features(in: ciimage)
         for feature in features {
             let qrFeature =  feature as! CIQRCodeFeature
-            print(qrFeature.messageString ?? "no message")
-            print(qrFeature.bounds)
+//            print(qrFeature.messageString ?? "no message")
+//            print(qrFeature.bounds)
+            results.append(qrFeature.messageString!)
+            
+            let resultImage = drawFrame(sourceImage: sourceImage, feature: qrFeature)
+            sourceIconView.image = resultImage
         }
+        
+        let alertVC = UIAlertController(title: "结果", message: results.description, preferredStyle: .alert)
+        let action = UIAlertAction(title: "关闭", style: .default) { (_) in
+            self.dismiss(animated: true, completion: nil)
+        }
+        alertVC.addAction(action)
+        present(alertVC, animated: true, completion: nil)
+    }
+}
+
+extension DetectorQRcodeViewController {
+    /// 绘制识别区
+    private func drawFrame(sourceImage: UIImage, feature: CIQRCodeFeature) -> UIImage? {
+        let size = sourceImage.size
+        
+        // 1. 开启图形上下文
+        UIGraphicsBeginImageContext(size)
+        
+        // 2. 绘制大图片
+        sourceImage.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        
+        // 坐标系转换（上下颠倒）
+        let context = UIGraphicsGetCurrentContext()
+        context?.scaleBy(x: 1, y: -1)
+        context?.translateBy(x: 0, y: -size.height)
+        
+        // 3. 绘制路径
+        let bounds = feature.bounds
+        let path = UIBezierPath(rect: bounds)
+        UIColor.red.setStroke()
+        path.lineWidth = 6
+        path.stroke()
+        
+        // 4. 取出结果图片
+        let resultImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        // 5. 关闭上下文
+        UIGraphicsEndImageContext()
+        
+        return resultImage
     }
 }
