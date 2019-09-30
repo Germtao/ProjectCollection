bee0d8589271d04583a967e40985f20c3766f8c8
QDSG <piftao1127@163.com>
2019-09-30 18:26:40 +0800

    04. 快速聊天菜单

---
+++ 30DaysOfSwift/04 - SnapChatMenu/SnapChatMenu/SnapChatMenu/CameraViewController.swift
@@ -0,0 +1,113 @@
+//
+//  CameraViewController.swift
+//  SnapChatMenu
+//
+//  Created by QDSG on 2019/9/30.
+//  Copyright © 2019 unitTao. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraViewController: UIViewController {
+    
+    private var previewLayer: AVCaptureVideoPreviewLayer?
+    private var session: AVCaptureSession?
+    private var output: AVCapturePhotoOutput?
+    
+    private var didTakePhoto: Bool = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        initCamera()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer?.frame = view.bounds
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        didPressedTakeAnother()
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
+
+extension CameraViewController {
+    /// 初始化相机
+    private func initCamera() {
+        session = AVCaptureSession()
+        session?.sessionPreset = .hd1920x1080
+        var backCamera: AVCaptureDevice
+        if #available(iOS 10.0, *) {
+            backCamera = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera], mediaType: .video, position: .back).devices.first!
+        } else {
+            backCamera = AVCaptureDevice.devices().first!
+        }
+        var error: Error?
+        var input: AVCaptureDeviceInput!
+        
+        do {
+            input = try AVCaptureDeviceInput(device: backCamera)
+        } catch let error1 {
+            error = error1
+            input = nil
+        }
+        
+        if error == nil && session?.canAddInput(input) != nil {
+            session?.addInput(input)
+            
+            output = AVCapturePhotoOutput()
+            
+            if let tempOutput = output, session?.canAddOutput(tempOutput) != nil {
+                session?.addOutput(tempOutput)
+                if let tempSession = session {
+                    previewLayer = AVCaptureVideoPreviewLayer(session: tempSession)
+                    previewLayer?.videoGravity = AVLayerVideoGravity.resizeAspect
+                    previewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+                    view.layer.addSublayer(previewLayer!)
+                    tempSession.startRunning()
+                }
+            }
+        }
+    }
+}
+
+extension CameraViewController: AVCapturePhotoCaptureDelegate {
+    private func didPressedTakeAnother() {
+        if didTakePhoto {
+            imageView.isHidden = true
+            didTakePhoto = false
+        } else {
+            session?.startRunning()
+            didTakePhoto = true
+            didPressedTakePhoto()
+        }
+    }
+    
+    private func didPressedTakePhoto() {
+        if let videoConnection = output?.connection(with: .video) {
+            videoConnection.videoOrientation = .portrait
+            output?.capturePhoto(with: AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg]), delegate: self)
+            
+        }
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photoSampleBuffer: CMSampleBuffer?, previewPhoto previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+        
+        if previewPhotoSampleBuffer != nil {
+        }
+        
+    }
+}
---
+++ 30DaysOfSwift/04 - SnapChatMenu/SnapChatMenu/SnapChatMenu/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  SnapChatMenu
+//
+//  Created by QDSG on 2019/9/30.
+//  Copyright © 2019 unitTao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private let width = UIScreen.main.bounds.width
+    private let height = UIScreen.main.bounds.height
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        scrollView.contentSize = CGSize(width: width * 3, height: scrollView.bounds.height)
+        scrollView.contentOffset = CGPoint(x: width, y: 0)
+    }
+}
+
+extension ViewController {
+    private func setupUI() {
+        setLeftView()
+        setCenterView()
+        setRightView()
+    }
+    
+    private func setLeftView() {
+        let leftView = UIView(frame: CGRect(x: 0,
+                                            y: 0,
+                                            width: width,
+                                            height: scrollView.bounds.height))
+        let imageView = UIImageView(frame: CGRect(x: 0,
+                                                  y: 0,
+                                                  width: leftView.bounds.width,
+                                                  height: leftView.bounds.height))
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "leftImage")
+        leftView.addSubview(imageView)
+        scrollView.addSubview(leftView)
+    }
+    
+    private func setCenterView() {
+        let cameraVc = storyboard!.instantiateViewController(identifier: "cameraVcId") as! CameraViewController
+        cameraVc.view.frame = CGRect(x: width,
+                                     y: 0,
+                                     width: width,
+                                     height: scrollView.bounds.height)
+        scrollView.addSubview(cameraVc.view)
+    }
+    
+    private func setRightView() {
+        let rightView = UIView(frame: CGRect(x: width * 2,
+                                             y: 0,
+                                             width: width,
+                                             height: scrollView.bounds.height))
+        let imageView = UIImageView(frame: CGRect(x: 0,
+                                                  y: 0,
+                                                  width: rightView.bounds.width,
+                                                  height: rightView.bounds.height))
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "rightImage")
+        rightView.addSubview(imageView)
+        scrollView.addSubview(rightView)
+    }
+}
