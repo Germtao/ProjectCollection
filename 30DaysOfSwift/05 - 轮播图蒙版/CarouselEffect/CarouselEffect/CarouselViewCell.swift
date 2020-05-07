fa44c1879d32791d3368bb8b4f5dad61bcb677bd
QDSG <piftao1127@163.com>
2020-05-07 18:24:08 +0800

    todo：轮播图蒙版

---
+++ 30DaysOfSwift/05 - 轮播图蒙版/CarouselEffect/CarouselEffect/CarouselViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CarouselViewCell.swift
+//  CarouselEffect
+//
+//  Created by QDSG on 2020/5/7.
+//  Copyright © 2020 tTao. All rights reserved.
+//
+
+import UIKit
+
+class CarouselViewCell: UICollectionViewCell {
+    
+}
---
+++ 30DaysOfSwift/05 - 轮播图蒙版/CarouselEffect/CarouselEffect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarouselEffect
+//
+//  Created by QDSG on 2020/5/7.
+//  Copyright © 2020 tTao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
