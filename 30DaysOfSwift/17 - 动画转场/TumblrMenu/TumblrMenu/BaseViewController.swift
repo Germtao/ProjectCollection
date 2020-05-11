2073eb43e849757f51eb28a9cdd342bd9c451921
QDSG <piftao1127@163.com>
2020-05-11 18:38:24 +0800

    todo：转场动画

---
+++ 30DaysOfSwift/17 - 动画转场/TumblrMenu/TumblrMenu/BaseViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BaseViewController.swift
+//  TumblrMenu
+//
+//  Created by QDSG on 2020/5/11.
+//  Copyright © 2020 tTao. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    
+    
+}
+
---
+++ 30DaysOfSwift/17 - 动画转场/TumblrMenu/TumblrMenu/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  TumblrMenu
+//
+//  Created by QDSG on 2020/5/11.
+//  Copyright © 2020 tTao. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
