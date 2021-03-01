7fd3e1eecedde3da99bec7cbcb686afebc670c1a
QDSG <piftao1127@163.com>
2021-03-01 18:09:20 +0800

    Utils

--- TTXMLY/TTXMLY/Core/Utils/Utils.swift
+++ TTXMLY/TTXMLY/Core/Utils/Utils.swift
@@ -8,42 +8,46 @@
 
 import UIKit
 
-struct Size {
-    static let screenW: CGFloat = UIScreen.main.bounds.width
-    static let screenH: CGFloat = UIScreen.main.bounds.height
-    static let navBarH: CGFloat = Size.specialScreen ? 88.0 : 64.0
-    static let tabBarH: CGFloat = Size.specialScreen ? 83.0 : 49.0
-    static let statusBarH: CGFloat = Size.specialScreen ? 44.0 : 20.0
+struct Constants {
+    struct Colors {
+        static let defaultTitle = UIColor.black
+        static let defaultBackground = UIColor.white
+        static let line = UIColor(red: (218.0/255.0), green: (218.0/255.0), blue: (218.0/255.0), alpha: 1.0)
+        static let navBarTint = UIColor(red: 245/255.0, green: 245/255.0, blue: 245/255.0, alpha: 1)
+        static let button = UIColor(red: 242/255.0, green: 77/255.0, blue: 51/255.0, alpha: 1)
+        static let buttonBg = UIColor(red: 254/255.0, green: 232/255.0, blue: 227/255.0, alpha: 1)
+        
+        static let sectionFooter = UIColor(red: 240/255.0, green: 241/255.0, blue: 244/255.0, alpha: 1)
+        
+        static let endLabel = UIColor(red: 248, green: 210, blue: 74, alpha: 1)
+        
+        static let categoryCell = UIColor(red: 220/255.0, green: 220/255.0, blue: 220/255.0, alpha: 1)
+        
+        static let vipFreeBg = UIColor(red: 203/255.0, green: 148/255.0, blue: 95/255.0, alpha: 1)
+    }
     
-    /// 是否为刘海屏
-    static let specialScreen: Bool = Size.screenH >= 812.0 ? true : false
+    struct Fonts {
+        static let defaultFont = font(18)
+        
+        static func font(_ size: CGFloat) -> UIFont {
+            return UIFont.systemFont(ofSize: size)
+        }
+    }
     
-    static let defaultFontSize: CGFloat = 18.0
+    struct Sizes {
+        static let screenW: CGFloat = UIScreen.main.bounds.width
+        static let screenH: CGFloat = UIScreen.main.bounds.height
+        static let navBarH: CGFloat = Bools.specialScreen ? 88.0 : 64.0
+        static let tabBarH: CGFloat = Bools.specialScreen ? 83.0 : 49.0
+        static let statusBarH: CGFloat = Bools.specialScreen ? 44.0 : 20.0
+        
+        // MARK: - 首页推荐
+        static let newsCellMargin: CGFloat = 150.0
+    }
     
-    // MARK: - 首页推荐
-    static let newsCellMargin: CGFloat = 150.0
-}
-
-struct Color {
-    static let defaultTitle = UIColor.black
-    static let defaultBackground = UIColor.white
-    static let line = UIColor(red: (218.0/255.0), green: (218.0/255.0), blue: (218.0/255.0), alpha: 1.0)
-    static let navBarTint = UIColor(red: 245/255.0, green: 245/255.0, blue: 245/255.0, alpha: 1)
-    static let button = UIColor(red: 242/255.0, green: 77/255.0, blue: 51/255.0, alpha: 1)
-    static let buttonBg = UIColor(red: 254/255.0, green: 232/255.0, blue: 227/255.0, alpha: 1)
-    
-    static let sectionFooter = UIColor(red: 240/255.0, green: 241/255.0, blue: 244/255.0, alpha: 1)
-    
-    static let endLabel = UIColor(red: 248, green: 210, blue: 74, alpha: 1)
-    
-    static let categoryCell = UIColor(red: 220/255.0, green: 220/255.0, blue: 220/255.0, alpha: 1)
-    
-    static let vipFreeBg = UIColor(red: 203/255.0, green: 148/255.0, blue: 95/255.0, alpha: 1)
-}
-
-struct TTFont {
-    static func font(_ size: CGFloat) -> UIFont {
-        return UIFont.systemFont(ofSize: size)
+    struct Bools {
+        /// 是否为刘海屏
+        static let specialScreen: Bool = Sizes.screenH >= 812.0 ? true : false
     }
 }
 
