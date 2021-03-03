d99cb898a6a697f5ace41460b9041dab4f8a6c58
QDSG <piftao1127@163.com>
2021-03-03 11:47:52 +0800

    首页推荐请求

--- TTXMLY/TTXMLY/Modules/Home(首页)/Recommend(推荐)/ViewModel/TTFMRecommendAPI.swift
+++ TTXMLY/TTXMLY/Modules/Home(首页)/Recommend(推荐)/ViewModel/TTFMRecommendAPI.swift
@@ -70,90 +70,98 @@ extension TTFMRecommendAPI: TargetType {
     var method: Moya.Method { return .get }
     
     var task: Task {
-        var paras: [String: Any] = [:]
-        switch self {
-        case .recommendList:
-            paras = ["device": "iPhone",
-                     "appid": 0,
-                     "categoryId": -2,
-                     "channel": "ios-b1",
-                     "code": "43_310000_3100",
-                     "includeActivity": true,
-                     "includeSpecial": true,
-                     "network": "WIFI",
-                     "operator": 3,
-                     "pullToRefresh": true,
-                     "scale": 3,
-                     "uid": 0,
-                     "version": "6.5.3",
-                     "xt": Int32(Date().timeIntervalSince1970),
-                     "deviceId": UIDevice.current.identifierForVendor!.uuidString]
-            
-        case .recommendListForU:
-            paras = ["device": "iPhone",
-                     "excludedAlbumIds": "3144231%2C3160816%2C5088879%2C3703879%2C9723091%2C12580785%2C12610571%2C13507836%2C11501536%2C12642314%2C4137349%2C10587045%2C6233693%2C4436043%2C16302530%2C15427120%2C13211141%2C61%2C220565%2C3475911%2C3179882%2C10596891%2C261506%2C7183288%2C203355%2C3493173%2C7054736%2C10728301%2C2688602%2C13048404",
-                     "appid": 0,
-                     "categoryId": -2,
-                     "pageId": 1,
-                     "pageSize": 20,
-                     "network": "WIFI",
-                     "operator": 3,
-                     "scale": 3,
-                     "uid": 124057809,
-                     "version": "6.5.3",
-                     "xt": Int32(Date().timeIntervalSince1970),
-                     "deviceId": UIDevice.current.identifierForVendor!.uuidString]
-        case .recommendListWithAd:
-            paras = ["device": "iPhone",
-                     "appid": 0,
-                     "name":"find_native",
-                     "network":"WIFI",
-                     "operator":3,
-                     "scale":3,
-                     "version":"6.5.3",
-                     "xt": Int32(Date().timeIntervalSince1970)]
-        case .guessULikeMoreList:
-            paras = ["device": "iPhone",
-                     "appid": 0,
-                     "inreview": false,
-                     "network":"WIFI",
-                     "operator": 3,
-                     "pageId": 1,
-                     "pageSize": 20,
-                     "scale":3,
-                     "uid": 124057809,
-                     "version": "6.5.3",
-                     "xt": Int32(Date().timeIntervalSince1970),
-                     "deviceId": UIDevice.current.identifierForVendor!.uuidString]
-        case .changeGuessULikeList:
-            paras = ["device": "iPhone",
-                     "appid": 0,
-                     "excludedAdAlbumIds": "8258116%2C8601255%2C16514340",
-                     "excludedAlbumIds": "4169245%2C4156778%2C4078652%2C8601255%2C4177638%2C16514340%2C5993267%2C12201334%2C13089888%2C4310827%2C4792267%2C2912127%2C13403391%2C4193171%2C5411224%2C8258116%2C4323493%2C10829913",
-                     "excludedRoomIds": "",
-                     "excludedSpecialIds": "",
-                     "excludedOffset": 18,
-                     "inreview": false,
-                     "loopIndex": 3,
-                     "network":"WIFI",
-                     "operator": 3,
-                     "pageId": 1,
-                     "pageSize": 6,
-                     "scale":3,
-                     "uid": 0,
-                     "version": "6.5.3",
-                     "xt": Int32(Date().timeIntervalSince1970),
-                     "deviceId": UIDevice.current.identifierForVendor!.uuidString]
-        default:
-            break
-        }
-        
-        return .requestParameters(parameters: paras, encoding: URLEncoding.default)
+        return .requestParameters(parameters: params, encoding: URLEncoding.default)
     }
     
     var sampleData: Data { return "".data(using: .utf8)! }
     
     var headers: [String : String]? { return nil }
     
-    
+    private var params: [String: Any] {
+        switch self {
+        case .recommendList:
+            return [
+                "device": "iPhone",
+                "appid": 0,
+                "categoryId": -2,
+                "channel": "ios-b1",
+                "code": "43_310000_3100",
+                "includeActivity": true,
+                "includeSpecial": true,
+                "network": "WIFI",
+                "operator": 3,
+                "pullToRefresh": true,
+                "scale": 3,
+                "uid": 0,
+                "version": "6.5.3",
+                "xt": Int32(Date().timeIntervalSince1970),
+                "deviceId": UIDevice.current.identifierForVendor!.uuidString
+            ]
+        case .recommendListForU:
+            return [
+                "device": "iPhone",
+                "excludedAlbumIds": "3144231%2C3160816%2C5088879%2C3703879%2C9723091%2C12580785%2C12610571%2C13507836%2C11501536%2C12642314%2C4137349%2C10587045%2C6233693%2C4436043%2C16302530%2C15427120%2C13211141%2C61%2C220565%2C3475911%2C3179882%2C10596891%2C261506%2C7183288%2C203355%2C3493173%2C7054736%2C10728301%2C2688602%2C13048404",
+                "appid": 0,
+                "categoryId": -2,
+                "pageId": 1,
+                "pageSize": 20,
+                "network": "WIFI",
+                "operator": 3,
+                "scale": 3,
+                "uid": 124057809,
+                "version": "6.5.3",
+                "xt": Int32(Date().timeIntervalSince1970),
+                "deviceId": UIDevice.current.identifierForVendor!.uuidString
+            ]
+        case .recommendListWithAd:
+            return [
+                "device": "iPhone",
+                "appid": 0,
+                "name":"find_native",
+                "network":"WIFI",
+                "operator":3,
+                "scale":3,
+                "version":"6.5.3",
+                "xt": Int32(Date().timeIntervalSince1970)
+            ]
+        case .guessULikeMoreList:
+            return [
+                "device": "iPhone",
+                 "appid": 0,
+                 "inreview": false,
+                 "network":"WIFI",
+                 "operator": 3,
+                 "pageId": 1,
+                 "pageSize": 20,
+                 "scale":3,
+                 "uid": 124057809,
+                 "version": "6.5.3",
+                 "xt": Int32(Date().timeIntervalSince1970),
+                 "deviceId": UIDevice.current.identifierForVendor!.uuidString
+            ]
+        case .changeGuessULikeList:
+            return [
+                "device": "iPhone",
+                 "appid": 0,
+                 "excludedAdAlbumIds": "8258116%2C8601255%2C16514340",
+                 "excludedAlbumIds": "4169245%2C4156778%2C4078652%2C8601255%2C4177638%2C16514340%2C5993267%2C12201334%2C13089888%2C4310827%2C4792267%2C2912127%2C13403391%2C4193171%2C5411224%2C8258116%2C4323493%2C10829913",
+                 "excludedRoomIds": "",
+                 "excludedSpecialIds": "",
+                 "excludedOffset": 18,
+                 "inreview": false,
+                 "loopIndex": 3,
+                 "network":"WIFI",
+                 "operator": 3,
+                 "pageId": 1,
+                 "pageSize": 6,
+                 "scale":3,
+                 "uid": 0,
+                 "version": "6.5.3",
+                 "xt": Int32(Date().timeIntervalSince1970),
+                 "deviceId": UIDevice.current.identifierForVendor!.uuidString
+            ]
+        default:
+            return [:]
+        }
+    }
 }
