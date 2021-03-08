80dea5c2617829daf2dea2f4d03942002b1c6d35
QDSG <piftao1127@163.com>
2021-03-08 11:53:45 +0800

    base request class

---
+++ TTXMLY_OC/TTXMLY_OC/Net/Base/TTBaseRequest.h
@@ -0,0 +1,34 @@
+//
+//  TTBaseRequest.h
+//  TTXMLY_OC
+//
+//  Created by QDSG on 2021/3/8.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef NS_ENUM(NSInteger, TTHttpType) {
+    TTHttpType_GET,
+    TTHttpType_POST,
+};
+
+typedef void(^TTHttpRequestCompletion)(id _Nullable responseObject,NSString * _Nullable message, BOOL success);
+
+@interface TTBaseRequest : NSObject
+
+/// 设置请求地址
++ (instancetype)requestWithURL:(NSString *)url;
+
+/// 发送网络请求
+/// @param methodType 请求方式 GET POST
+/// @param params     请求参数
+/// @param completion 请求完成回调
+- (NSURLSessionDataTask *)startWithMethod:(TTHttpType)methodType
+                                   params:(id)params
+                               completion:(TTHttpRequestCompletion)completion;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TTXMLY_OC/TTXMLY_OC/Net/Base/TTBaseRequest.m
@@ -0,0 +1,77 @@
+//
+//  TTBaseRequest.m
+//  TTXMLY_OC
+//
+//  Created by QDSG on 2021/3/8.
+//
+
+#import "TTBaseRequest.h"
+#import <AFNetworking/AFNetworking.h>
+
+@interface TTBaseRequest ()
+
+@property (nonatomic, copy) NSString *urlString;
+
+@property (nonatomic, copy) TTHttpRequestCompletion completionBlock;
+
+@property (nonatomic, strong) AFHTTPSessionManager *sessionManager;
+
+@end
+
+@implementation TTBaseRequest
+
++ (instancetype)requestWithURL:(NSString *)url {
+    TTBaseRequest *request = [[TTBaseRequest alloc] init];
+    request.urlString = url;
+    return request;
+}
+
+- (NSURLSessionDataTask *)startWithMethod:(TTHttpType)methodType
+                                   params:(id)params
+                               completion:(TTHttpRequestCompletion)completion {
+    NSURLSessionDataTask *task = nil;
+    self.completionBlock = completion;
+    self.sessionManager = [self sessionManagerWithParams:params];
+    NSString *urlString = [self.urlString stringByAddingPercentEncodingWithAllowedCharacters:[[NSCharacterSet characterSetWithCharactersInString:@"?!@#$^&%*+,:;='\"`<>()[]{}/\\| "] invertedSet]];
+    
+    if (methodType == TTHttpType_GET) {
+        task = [self.sessionManager GET:urlString
+                             parameters:params
+                                headers:nil
+                               progress:nil
+                                success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            if (self.completionBlock) {
+                self.completionBlock(responseObject, nil, YES);
+            }
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            if (self.completionBlock) {
+                self.completionBlock(nil, error.description, NO);
+            }
+        }];
+    } else if (methodType == TTHttpType_POST) {
+        task = [self.sessionManager POST:urlString
+                              parameters:params
+                                 headers:nil
+                                progress:nil
+                                 success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            if (self.completionBlock) {
+                self.completionBlock(responseObject, nil, YES);
+            }
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            if (self.completionBlock) {
+                self.completionBlock(nil, error.description, NO);
+            }
+        }];
+    }
+    return nil;
+}
+
+- (AFHTTPSessionManager *)sessionManagerWithParams:(NSDictionary *)params {
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    configuration.timeoutIntervalForRequest = 30.0f;
+    AFHTTPSessionManager *sessionManager = [[AFHTTPSessionManager alloc] initWithSessionConfiguration:configuration];
+    sessionManager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/plain",  @"text/json", @"text/javascript", @"text/html", nil];
+    return sessionManager;
+}
+
+@end
